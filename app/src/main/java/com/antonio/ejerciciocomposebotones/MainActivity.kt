37770601df3aa7d309944ef50089c38616c34951
Antonio Lorenzo Vizcaino <lorenzovizcaino@gmail.com>
2024-01-26 20:08:00 +0100

    26/01/24_20:07

--- app/src/main/java/com/antonio/ejerciciocomposebotones/MainActivity.kt
+++ app/src/main/java/com/antonio/ejerciciocomposebotones/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -33,6 +34,8 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.antonio.ejerciciocomposebotones.ui.theme.EjercicioComposeBotonesTheme
 
 class MainActivity : ComponentActivity() {
@@ -224,7 +227,8 @@ fun EjComposeBotones2() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val context = LocalContext.current
-        Button(onClick = {
+        Button(colors = ButtonDefaults.buttonColors(Color(103,80,164)),
+            onClick = {
             showToast("ButtonConButton", context)
         }) {
             Text(text = "ButtonConButton")
@@ -237,10 +241,14 @@ fun EjComposeBotones2() {
             }) {
             Text(text = "ButtonEnText", color = Color.Black)
         }
-        Button(onClick = {
+        Button(
+
+            colors=ButtonDefaults.buttonColors(Color.Green),
+            onClick = {
             showToast("ButtonEnBox", context)
-        }) {
-            Text(text = "ButtonEnBox")
+        }
+        ) {
+            Text(text = "ButtonEnBox", fontSize = 22.sp)
         }
     }
 
